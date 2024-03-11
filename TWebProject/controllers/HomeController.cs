4eb9845e0c4cf7d7fb8f08596184e63ab59a8355
Dumitras <dumitrufrimu118@gmail.com>
2024-03-11 19:04:21 +0200

    added home controller

--- TWebProject/controllers/HomeController.cs
+++ TWebProject/controllers/HomeController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TWebProject.Models;
 
 namespace TWebProject.Web.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
-        public ActionResult Index()
+    // GET: Home
+    public ActionResult Index()
         {
-            return View();
+            UserData u = new UserData();
+            u.Username = ;
+            u.Products = new List<string> {};
+        
+        return View(u);
         }
     }
-}
