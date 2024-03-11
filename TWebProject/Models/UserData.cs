3aad07000a495be7e0ca7fcffc88ca34ca1cb07d
Grigore Scutelnic <scutelnic.grigore10@gmail.com>
2024-03-11 18:56:42 +0200

    added UserData Class

---
+++ TWebProject/Models/UserData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TWebProject.Models
+{
+    public class UserData
+    {
+        public String Username {get; set; }
+        public List<String>Products { get; set; }
+    }
+}
