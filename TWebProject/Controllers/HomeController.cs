49aba65382be3cd1ba33781b88bea9b36926e1ec
Grigore Scutelnic <scutelnic.grigore10@gmail.com>
2024-03-11 19:53:21 +0200

    modif

---
+++ TWebProject/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TWebProject.Models;
+
+namespace TWebProject.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+    // GET: Home
+    public ActionResult Index()
+        {
+            UserData u = new UserData();
+            u.Username = "Customer";
+            u.Products = new List<string> {"Product #1", "Product #2", "Product #3", "Product #4"};
+        
+        return View(u);
+        }
+    }
+
