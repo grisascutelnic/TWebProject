6b0d942f8a7af9d1bc334aa15f8612b99de961e3
Grigore Scutelnic <scutelnic.grigore10@gmail.com>
2024-04-23 11:59:03 +0300

    Am creat entitatea User.cs din DbModel

---
+++ TWebProject.Domain/Entitie/User/DbModel/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TWebProject.Domain.Entitie.User.DbModel
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(30, MinimumLength = 8)]
+        public string Password { get; set; }
+        [Required]
+        public string UserName { get; set; }
+
+    }
+}
