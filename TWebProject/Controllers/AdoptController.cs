6ddb1b6da9899c5f3dc9585e8f893898e37a295d
Dumitras <dumitrufrimu118@gmail.com>
2024-05-30 04:36:23 +0300

    Am adaugat sectiune de Contul Meu

---
+++ TWebProject/Controllers/AdoptController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TWebProject.Controllers
+{
+    public class AdoptController : Controller
+    {
+        // GET: Adopt
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TWebProject/Controllers/ContulMeuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TWebProject.Controllers
+{
+    public class ContulMeuController : Controller
+    {
+        // GET: ContulMeu
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
