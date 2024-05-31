a71feabc4ea1a88b3bacd893d3d251b27d4876fd
Dumitras <dumitrufrimu118@gmail.com>
2024-05-31 20:15:08 +0300

    Adaugare managment

---
+++ TWebProject/Controllers/ManagementController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TWebProject.Controllers
+{
+    public class ManagementController : Controller
+    {
+        // GET: Management
+        public ActionResult Index()
+        {
+            ViewBag.ActivePage = "Management";
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            ViewBag.ActivePage = "Management";
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.ActivePage = "Management";
+            return View();
+        }
+
+        public ActionResult Delete(string name)
+        {
+            ViewBag.ActivePage = "Management";
+            ViewBag.Name = name;
+            return View();
+        }
+
+    }
+}
