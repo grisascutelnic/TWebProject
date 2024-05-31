2d645143db87401cf8afe20d688fb5d456c0e830
Grigore Scutelnic <scutelnic.grigore10@gmail.com>
2024-05-31 17:42:05 +0300

    pagina preferate

---
+++ TWebProject/Controllers/PreferateController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TWebProject.Controllers
+{
+    public class PreferateController : Controller
+    {
+        // GET: Preferate
+        public ActionResult Index()
+        {
+            ViewBag.ActivePage = "Preferate";
+            return View();
+        }
+    }
+}
